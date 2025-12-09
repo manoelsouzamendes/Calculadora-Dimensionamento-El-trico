c29f17cf50bd47e0fdc9d9ca56ebd0a242725a23
manoelsouzamendes <31786164+manoelsouzamendes@users.noreply.github.com>
2025-12-09 20:07:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -282,4 +282,3 @@ if st.session_state['dados_comodos'] and st.button("🚀 Calcular Dimensionament
             
     st.table(pd.DataFrame(resultados))
     st.success("Cálculo realizado com sucesso!")
-
